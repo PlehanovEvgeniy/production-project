fed424e2619b5a918f81da5179462d07a13c0140
eplekhanov <eplekhanov@phoenixit.ru>
2023-03-10 18:30:58 +0300

    Внедрение FSD архитектуры

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,10 @@
 import {Link, Route, Routes} from "react-router-dom";
-import {AboutPageLazy} from "./pages/AboutPage/AboutPage.lazy";
-import {MainPageLazy} from "./pages/MainPage/MainPageLazy";
 import {Suspense} from "react";
 import './styles/index.scss';
-import {useTheme} from "./theme/useTheme";
-import {classNames} from "./helpers/classNames/classNames";
+import {classNames} from "shared/lib/classNames/classNames";
+import {useTheme} from "app/providers/ThemeProvider";
+import {AboutPage} from "pages/AboutPage";
+import {MainPage} from "pages/MainPage";
 
 
 const App = () => {
@@ -17,8 +17,8 @@ const App = () => {
       <Link to={"/about"}>О сайте</Link>
       <Suspense fallback={<div>Loading</div>}>
         <Routes>
-          <Route path={"/about"} element={<AboutPageLazy/>}/>
-          <Route path={"/"} element={<MainPageLazy/>}/>
+          <Route path={"/about"} element={<AboutPage/>}/>
+          <Route path={"/"} element={<MainPage/>}/>
         </Routes>
       </Suspense>
     </div>
