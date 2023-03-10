908cdaa8f8131e82b070148f982f2b7dd3322010
eplekhanov <eplekhanov@phoenixit.ru>
2023-03-10 16:57:58 +0300

    Добавил роутинг

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,21 @@
-import React from "react";
-import { Routes } from "react-router-dom";
-import Counter from "./components/Counter";
+import {Link, Route, Routes} from "react-router-dom";
 import "./index.scss";
+import {AboutPageLazy} from "./pages/AboutPage/AboutPage.lazy";
+import {MainPageLazy} from "./pages/MainPage/MainPageLazy";
+import {Suspense} from "react";
+
 
 const App = () => {
   return (
     <div className="app">
-      <Routes path={'/about'}>
-
-      </Routes>
+      <Link to={"/"}>Главная</Link>
+      <Link to={"/about"}>О сайте</Link>
+      <Suspense fallback={<div>Loading</div>}>
+        <Routes>
+          <Route path={"/about"} element={<AboutPageLazy/>}/>
+          <Route path={"/"} element={<MainPageLazy/>}/>
+        </Routes>
+      </Suspense>
     </div>
   );
 };
